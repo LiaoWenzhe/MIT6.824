67d0fcf06b5c41f0c43d8025adafb3d7fff4320e
LiaoWenzhe <45705519+LiaoWenzhe@users.noreply.github.com>
2021-07-24 09:57:08 +0800

    Update rpc.go

--- lab1/rpc.go
+++ lab1/rpc.go
@@ -1,1 +1,69 @@
+package mr
 
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+const (
+	MAP = "MAP"
+	REDUCE = "REDUCE"
+)
+
+type Task struct {
+	Type         string // MAP or REDUCE
+	Index        int
+	MapInputFile string
+
+	WorkerID string
+	Deadline time.Time
+}
+
+type ApplyForTaskArgs struct {
+	WorkerID string
+
+	LastTaskType  string
+	LastTaskIndex int
+}
+
+type ApplyForTaskReply struct {
+	TaskType     string // MAP or REDUCE
+	TaskIndex    int
+	MapInputFile string
+	MapNum       int
+	ReduceNum    int
+}
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the coordinator.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func coordinatorSock() string {
+	s := "/var/tmp/824-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
+
+func tmpMapOutFile(worker string, mapIndex int, reduceIndex int) string {
+	return fmt.Sprintf("tmp-worker-%s-%d-%d", worker, mapIndex, reduceIndex)
+}
+
+func finalMapOutFile(mapIndex int, reduceIndex int) string {
+	return fmt.Sprintf("mr-%d-%d", mapIndex, reduceIndex)
+}
+
+func tmpReduceOutFile(worker string, reduceIndex int) string {
+	return fmt.Sprintf("tmp-worker-%s-out-%d", worker, reduceIndex)
+}
+
+func finalReduceOutFile(reduceIndex int) string {
+	return fmt.Sprintf("mr-out-%d", reduceIndex)
+}
